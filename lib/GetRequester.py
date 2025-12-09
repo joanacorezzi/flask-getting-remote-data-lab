83d4a04a6820f45b3be5c32ef2ef8045dff398fe
Joana Corezzi Williams <joanacorezzi@gmail.com>
2025-12-09 11:44:49 -0600

    Add GetRequester class with get_response_body an JSON

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+
+        response = requests.get(self.url)  # make the HTTP request
+        return response.content             # return the response body as a string
+
 
     def load_json(self):
-        pass
+        
+        body = self.get_response_body()    # get the raw text from the API
+        data = json.loads(body)            # convert JSON text into Python data
+        return data                        # return the Python object 
